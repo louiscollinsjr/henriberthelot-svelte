f72d29fa02aaa69ff447eebbfa03ae9b7a8b8d22
louiscollinsjr <louiscollinsjr@gmail.com>
2025-04-21 13:59:19 +0300

    Update Vercel adapter and switch from auto to Vercel adapter with Node 22

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
